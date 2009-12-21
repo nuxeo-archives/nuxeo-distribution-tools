4ac54a57f40fe8ab031eea90c3159dbb15f08a77
Julien Carsique <jcarsique@nuxeo.com>
2009-12-21 15:54:31 +0100

    cleanup

--- src/main/java/org/nuxeo/build/maven/graph/Node.java
+++ src/main/java/org/nuxeo/build/maven/graph/Node.java
@@ -31,34 +31,40 @@ import org.apache.tools.ant.BuildException;
 import org.nuxeo.build.maven.filter.Filter;
 
 /**
- * TODO: use pom settings when resolving an artifact (use remote repos specified in pom if any)
- *
+ * TODO: use pom settings when resolving an artifact (use remote repos specified
+ * in pom if any)
+ * 
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
- *
+ * 
  */
 @SuppressWarnings("unchecked")
 public class Node {
 
     protected Graph graph;
+
     protected String id;
+
     protected Artifact artifact;
+
     protected List<Edge> edgesIn;
+
     protected List<Edge> edgesOut;
 
     private boolean isExpanded;
 
     /**
-     * Point to an artifact pom. When embedded in maven and using the current project pom
-     * as the root this will be set by the maven loader mojo to point to the current pom 
+     * Point to an artifact pom. When embedded in maven and using the current
+     * project pom as the root this will be set by the maven loader mojo to
+     * point to the current pom
      */
     protected MavenProject pom;
 
-
     public static String createNodeId(Artifact artifact) {
-        return new StringBuilder().append(artifact.getGroupId()).append(':').append(artifact.getArtifactId()).append(':').append(artifact.getVersion()).append(':').append(artifact.getType()).append(':').toString();
+        return new StringBuilder().append(artifact.getGroupId()).append(':').append(
+                artifact.getArtifactId()).append(':').append(
+                artifact.getVersion()).append(':').append(artifact.getType()).append(
+                ':').toString();
     }
-
-
 
     public Node(Node node) {
         this.graph = node.graph;
@@ -71,7 +77,7 @@ public class Node {
     }
 
     public Node(Graph graph, MavenProject pom, Artifact artifact) {
-        this (graph, pom, artifact, Node.createNodeId(artifact));
+        this(graph, pom, artifact, Node.createNodeId(artifact));
     }
 
     protected Node(Graph graph, MavenProject pom, Artifact artifact, String id) {
@@ -99,7 +105,8 @@ public class Node {
     public File getFile(String classifier) {
         resolveIfNeeded();
         Artifact ca = graph.maven.getArtifactFactory().createArtifactWithClassifier(
-                artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion(), artifact.getType(), classifier);
+                artifact.getGroupId(), artifact.getArtifactId(),
+                artifact.getVersion(), artifact.getType(), classifier);
         try {
             graph.maven.resolve(ca);
             File file = ca.getFile();
@@ -149,7 +156,7 @@ public class Node {
     public boolean isExpanded() {
         return isExpanded;
     }
-    
+
     public void expand(int recurse, Filter filter) {
         if (isExpanded) {
             return;
@@ -160,12 +167,16 @@ public class Node {
             return;
         }
         if (recurse > 0) {
-            loadDependencies(recurse-1, (List<Dependency>)pom.getDependencies(), filter);
-//            if ("pom".equals(artifact.getType())) {
-//                loadDependencies(recurse-1, (List<Dependency>)pom.getDependencyManagement().getDependencies(), filter);
-//            } else {
-//                loadDependencies(recurse-1, (List<Dependency>)pom.getDependencies(), filter);
-//            }
+            loadDependencies(recurse - 1,
+                    (List<Dependency>) pom.getDependencies(), filter);
+            // if ("pom".equals(artifact.getType())) {
+            // loadDependencies(recurse-1,
+            // (List<Dependency>)pom.getDependencyManagement().getDependencies(),
+            // filter);
+            // } else {
+            // loadDependencies(recurse-1,
+            // (List<Dependency>)pom.getDependencies(), filter);
+            // }
         }
     }
 
@@ -189,44 +200,51 @@ public class Node {
         return path;
     }
 
-
     public void resolveIfNeeded() {
         try {
             graph.getResolver().resolve(this);
         } catch (ArtifactNotFoundException e) {
-            throw new BuildException("Artifact not found: "+artifact.getId(), e);
+            throw new BuildException("Artifact not found: " + artifact.getId(),
+                    e);
+            // System.err.println("Artifact not found: "+artifact.getId());
+            // e.printStackTrace();
         }
     }
 
-    protected void loadDependencies(int recurse, List<Dependency> deps, Filter filter)  {
+    protected void loadDependencies(int recurse, List<Dependency> deps,
+            Filter filter) {
         resolveIfNeeded();
         ArtifactFactory factory = graph.getMaven().getArtifactFactory();
         MavenProject pom = getPom();
         if (pom == null) {
             return;
         }
-        for(Dependency d : deps) {
+        for (Dependency d : deps) {
             if (filter != null && !filter.accept(d)) {
                 continue;
             }
-            // the last boolean parameter is redundant, but the version that doesn't take this
+            // the last boolean parameter is redundant, but the version that
+            // doesn't take this
             // has a bug. See MNG-2524
-            Artifact a = factory.createDependencyArtifact(
-                    d.getGroupId(), d.getArtifactId(), VersionRange.createFromVersion(d.getVersion()),
+            Artifact a = factory.createDependencyArtifact(d.getGroupId(),
+                    d.getArtifactId(),
+                    VersionRange.createFromVersion(d.getVersion()),
                     d.getType(), d.getClassifier(), d.getScope(), false);
 
-            // beware of Maven bug! make sure artifact got the value inherited from dependency
+            // beware of Maven bug! make sure artifact got the value inherited
+            // from dependency
             assert a.getScope().equals(d.getScope());
             Node newNode = null;
             try {
                 newNode = graph.getNode(a);
             } catch (ArtifactNotFoundException e) {
-                throw new RuntimeException("Unable to find dependency: "+a+". Trail: "+getTrail(), e);
+                throw new RuntimeException("Unable to find dependency: " + a
+                        + ". Trail: " + getTrail(), e);
             }
             Edge edge = new Edge(this, newNode, d.getScope(), d.isOptional());
             addEdgeOut(edge);
             newNode.addEdgeIn(edge);
-            //edge.resolve(); //TODO resolve using pom repos
+            // edge.resolve(); //TODO resolve using pom repos
             if (recurse > 0) {
                 newNode.expand(recurse, filter);
             }
@@ -247,14 +265,13 @@ public class Node {
         }
     }
 
-
     @Override
     public boolean equals(Object obj) {
         if (obj == this) {
             return true;
         }
         if (obj instanceof Node) {
-            return ((Node)obj).id.equals(this);
+            return ((Node) obj).id.equals(this);
         }
         return false;
     }
